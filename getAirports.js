04fc0ca142dc3bd21a8732138b0246be8bde250c
Raphael Cockx <raphael@cockx.com>
2024-05-18 13:59:42 +0200

    Beautify output

--- getAirports.js
+++ getAirports.js
@@ -1,7 +1,9 @@
 import process from './process.js'
 
 import Bottleneck from 'bottleneck'
+import chalk from 'chalk'
 import got from 'got'
+import logUpdate from 'log-update'
 import write from 'write'
 
 // Throttle our calls
@@ -14,11 +16,15 @@ const letters = String.fromCharCode(...[...Array(26)].map((d, i) => i + 65))
 const data = []
 
 for (const letter of letters) {
-  console.log('Getting airports for IATA codes starting with', letter)
+  logUpdate(`● Getting airports for IATA codes starting with ${chalk.white.bold(letter)}`)
+
   const url = 'https://en.m.wikipedia.org/wiki/List_of_airports_by_IATA_airport_code:_' + letter
   const letterData = await limiter.schedule(() => got(url).then((response) => process.listOfAirports(response.body)))
 
+  logUpdate(`● Getting airports for IATA codes starting with ${chalk.white.bold(letter)} (${chalk.yellow.bold(letterData.length + ' entries found')})`)
+
   data.push(...letterData)
+  logUpdate.done()
 }
 
 const outputPath = new URL('./data/airports.json', import.meta.url).pathname
