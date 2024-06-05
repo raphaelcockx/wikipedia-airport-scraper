88bc2f91812286c112b7184b3dee682b424dbb6c
Raphael Cockx <raphael@cockx.com>
2024-06-05 13:42:37 +0200

    Detect tables in $adSection that aren't actually showing airlines and destinations

--- index.js
+++ index.js
@@ -10,122 +10,129 @@ const airportPage = function (body) {
   const $passengerHeading = $('h3 span.mw-headline#Passenger', $adSection)
 
   const $passengerTable = $passengerHeading.length > 0
-    ? $passengerHeading.parent().nextUntil('h3').filter('table')
+    ? $passengerHeading.parent().nextUntil('h3').filter('table.wikitable')
     : $('table.wikitable', $adSection).eq(0)
 
-  const $rows = $('tbody tr:not(:has(th))', $passengerTable)
+  // Check headings to make sure we have the right table
+  const isCorrectTable = $('th', $passengerTable).eq(0).text() === 'Airlines' && $('th', $passengerTable).eq(1).text() === 'Destinations'
 
-  return $rows.map(function () {
-    const $cols = $('td', $(this))
-    const $airlineCol = $cols.eq(0)
-    const $destinationsCol = $cols.eq(1)
+  if (isCorrectTable) {
+    const $rows = $('tbody tr:not(:has(th))', $passengerTable)
 
-    // Get airline data
-    const $airlineLink = $('a[title]', $airlineCol)
-    const rawAirlineLink = $airlineLink.attr('href') || null
-    const hasNoPage = /action=edit/.test(rawAirlineLink)
-    const airlineLink = hasNoPage || rawAirlineLink === null ? null : rawAirlineLink.replace('/wiki/', '')
+    return $rows.map(function () {
+      const $cols = $('td', $(this))
+      const $airlineCol = $cols.eq(0)
+      const $destinationsCol = $cols.eq(1)
 
-    const airline = {
-      name: $airlineLink.text() || $('span.nowrap', $airlineCol).text() || $airlineCol.text().replace(/\[[0-9a-z]{1,}\]/, '').trim(),
-      link: airlineLink
-    }
+      // Get airline data
+      const $airlineLink = $('a[title]', $airlineCol)
+      const rawAirlineLink = $airlineLink.attr('href') || null
+      const hasNoPage = /action=edit/.test(rawAirlineLink)
+      const airlineLink = hasNoPage || rawAirlineLink === null ? null : rawAirlineLink.replace('/wiki/', '')
 
-    const destinationsNodes = $destinationsCol.contents()
-      .map(function () {
-        let tagName = $(this).prop('tagName') || null
-        const rawAirportLink = $(this).attr('href') || null
-        const hasNoPage = /action=edit/.test(rawAirportLink)
-        const airportLink = hasNoPage || rawAirportLink === null ? null : rawAirportLink.replace('/wiki/', '')
-        let value = this.nodeValue ? this.nodeValue.trim() : $(this).text()
+      const airline = {
+        name: $airlineLink.text() || $('span.nowrap', $airlineCol).text() || $airlineCol.text().replace(/\[[0-9a-z]{1,}\]/, '').trim(),
+        link: airlineLink
+      }
 
-        // Some airports will be listed with no link. We should try to detect those cases
-        if (tagName === null && /^,\s?[a-zA-Z]+/.test(value)) {
-          tagName = 'A'
-          value = value.replace(',', '').trim()
-        }
+      const destinationsNodes = $destinationsCol.contents()
+        .map(function () {
+          let tagName = $(this).prop('tagName') || null
+          const rawAirportLink = $(this).attr('href') || null
+          const hasNoPage = /action=edit/.test(rawAirportLink)
+          const airportLink = hasNoPage || rawAirportLink === null ? null : rawAirportLink.replace('/wiki/', '')
+          let value = this.nodeValue ? this.nodeValue.trim() : $(this).text()
 
-        if (tagName === 'P') { // Content shouldn't be wrapped in a paragraph tag, but if it is...
-          return $(this).contents().map(function () {
-            const tagName = $(this).prop('tagName') || null
-            const link = $(this).attr('href')?.replace('/wiki/', '') || null
-            const value = this.nodeValue ? this.nodeValue.trim() : $(this).text()
+          // Some airports will be listed with no link. We should try to detect those cases
+          if (tagName === null && /^,\s?[a-zA-Z]+/.test(value)) {
+            tagName = 'A'
+            value = value.replace(',', '').trim()
+          }
 
+          if (tagName === 'P') { // Content shouldn't be wrapped in a paragraph tag, but if it is...
+            return $(this).contents().map(function () {
+              const tagName = $(this).prop('tagName') || null
+              const link = $(this).attr('href')?.replace('/wiki/', '') || null
+              const value = this.nodeValue ? this.nodeValue.trim() : $(this).text()
+
+              return {
+                tagName,
+                link,
+                value
+              }
+            }).get()
+          } else {
             return {
               tagName,
-              link,
+              link: airportLink,
               value
             }
-          }).get()
-        } else {
-          return {
-            tagName,
-            link: airportLink,
-            value
           }
+        })
+        .get()
+        .filter((d) => !['BR', 'SUP'].includes(d.tagName))
+        .filter(({ tagName, value }) => !(tagName === null && ['', ',', ', '].includes(value)))
+        .map((d, index) => ({ index, ...d }))
+        .reduce((acc, curr, i, arr) => {
+          if (i === 0 || curr.tagName === 'B') acc.push([])
+          acc[acc.length - 1].push(curr)
+
+          return acc
+        }, [])
+        .flatMap((nodes, blockIndex) => nodes.map((node) => ({ ...node, blockIndex })))
+
+      // Get markers and modifiers
+      const markers = destinationsNodes.filter((node) => node.tagName === 'B')
+      const modifiers = destinationsNodes
+        .filter((node) => node.tagName === null)
+        .map((modifier) => {
+          return {
+            ...modifier,
+            formattedDate: dateRegex.test(modifier.value) ? dayjs(modifier.value.match(dateRegex)[0]).format('YYYY-MM-DD') : null
+          }
+        })
+
+      // Get airports
+      const airportEntries = destinationsNodes.filter((node) => node.tagName === 'A')
+
+      const destinations = airportEntries.map((airport) => {
+        const { index, blockIndex, value: name, link } = airport
+
+        // Process markers
+        const isCharter = markers.find((marker) => marker.value === 'Charter:' && marker.blockIndex === blockIndex) !== undefined ||
+          markers.find((marker) => marker.value === 'Seasonal charter:' && marker.blockIndex === blockIndex) !== undefined
+
+        const isSeasonal = markers.find((marker) => marker.value === 'Seasonal:' && marker.blockIndex === blockIndex) !== undefined ||
+          markers.find((marker) => marker.value === 'Seasonal charter:' && marker.blockIndex === blockIndex) !== undefined
+
+        // Process modifiers
+        const suspended = modifiers.find((modifier) => /\((temporarily )?suspended/.test(modifier.value) && modifier.index === index + 1) !== undefined
+        const startDate = (modifiers.find((modifier) => /\((begins|resumes)/.test(modifier.value) && modifier.index === index + 1)?.formattedDate || null) ||
+          (modifiers.find((modifier) => /\((both begin|both resume)/.test(modifier.value) && modifier.blockIndex === blockIndex)?.formattedDate || null)
+
+        const endDate = modifiers.find((modifier) => /\((ends)/.test(modifier.value) && modifier.index === index + 1)?.formattedDate || null
+
+        const destination = {
+          name,
+          link,
+          isCharter,
+          isSeasonal,
+          suspended,
+          startDate,
+          endDate
         }
-      })
-      .get()
-      .filter((d) => !['BR', 'SUP'].includes(d.tagName))
-      .filter(({ tagName, value }) => !(tagName === null && ['', ',', ', '].includes(value)))
-      .map((d, index) => ({ index, ...d }))
-      .reduce((acc, curr, i, arr) => {
-        if (i === 0 || curr.tagName === 'B') acc.push([])
-        acc[acc.length - 1].push(curr)
 
-        return acc
-      }, [])
-      .flatMap((nodes, blockIndex) => nodes.map((node) => ({ ...node, blockIndex })))
-
-    // Get markers and modifiers
-    const markers = destinationsNodes.filter((node) => node.tagName === 'B')
-    const modifiers = destinationsNodes
-      .filter((node) => node.tagName === null)
-      .map((modifier) => {
         return {
-          ...modifier,
-          formattedDate: dateRegex.test(modifier.value) ? dayjs(modifier.value.match(dateRegex)[0]).format('YYYY-MM-DD') : null
+          airline,
+          destination
         }
       })
 
-    // Get airports
-    const airportEntries = destinationsNodes.filter((node) => node.tagName === 'A')
-
-    const destinations = airportEntries.map((airport) => {
-      const { index, blockIndex, value: name, link } = airport
-
-      // Process markers
-      const isCharter = markers.find((marker) => marker.value === 'Charter:' && marker.blockIndex === blockIndex) !== undefined ||
-        markers.find((marker) => marker.value === 'Seasonal charter:' && marker.blockIndex === blockIndex) !== undefined
-
-      const isSeasonal = markers.find((marker) => marker.value === 'Seasonal:' && marker.blockIndex === blockIndex) !== undefined ||
-        markers.find((marker) => marker.value === 'Seasonal charter:' && marker.blockIndex === blockIndex) !== undefined
-
-      // Process modifiers
-      const suspended = modifiers.find((modifier) => /\((temporarily )?suspended/.test(modifier.value) && modifier.index === index + 1) !== undefined
-      const startDate = (modifiers.find((modifier) => /\((begins|resumes)/.test(modifier.value) && modifier.index === index + 1)?.formattedDate || null) ||
-        (modifiers.find((modifier) => /\((both begin|both resume)/.test(modifier.value) && modifier.blockIndex === blockIndex)?.formattedDate || null)
-
-      const endDate = modifiers.find((modifier) => /\((ends)/.test(modifier.value) && modifier.index === index + 1)?.formattedDate || null
-
-      const destination = {
-        name,
-        link,
-        isCharter,
-        isSeasonal,
-        suspended,
-        startDate,
-        endDate
-      }
-
-      return {
-        airline,
-        destination
-      }
-    })
-
-    return destinations
-  }).toArray()
+      return destinations
+    }).toArray()
+  } else {
+    return []
+  }
 }
 
 const listOfAirports = function (body) {
