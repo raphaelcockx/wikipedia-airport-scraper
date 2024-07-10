2bd6ac70eafee9ab26c0e96e68b3101a1d1bd93b
Raphael Cockx <raphael@cockx.com>
2024-07-10 17:02:15 +0200

    Take into account more $adSection variations

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const dateRegex = /([0-9]{1,2} )?(January|February|March|April|May|June|July|Aug
 const airportPage = function (body) {
   const $ = cheerio.load(body)
 
-  const $adSection = $('span.mw-headline#Airlines_and_destinations, span.mw-headline#Airline_and_destination, span.mw-headline#Airline_and_destinations').parent().next()
+  const $adSection = $([
+    'span.mw-headline#Airlines_and_destinations',
+    'span.mw-headline#Airline_and_destination',
+    'span.mw-headline#Airline_and_destinations',
+    'h2#Airlines_and_destinations',
+    'h2#Airline_and_destination',
+    'h2#Airline_and_destinations'
+  ].join(', ')).parent().next()
+
   const $passengerHeading = $('h3 span.mw-headline#Passenger', $adSection)
 
   const $passengerTable = $passengerHeading.length > 0
